8897e273398f4868900f5a84cc66dfb1c9ba77f5
Sai Rohit <rrrohit61@gmail.com>
2023-07-15 18:54:00 -0500

    Added Angular route to nav links

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -16,7 +17,7 @@ export class NavComponent implements OnInit {
   // so we can have access in the templateurl - html
   // currentUser$: Observable<User | null> = of(null);
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
     // this.currentUser$ = this.accountService.currentUser$
@@ -24,9 +25,7 @@ export class NavComponent implements OnInit {
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: res => {
-        console.log(res);
-      },
+      next: () => this.router.navigateByUrl('/members'),
       error: err => console.log(err) 
     })
 
@@ -35,6 +34,7 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
