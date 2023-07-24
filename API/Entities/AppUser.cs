a7fb28321bc667a222355e32be353236e1d7f363
Sai Rohit <rrrohit61@gmail.com>
2023-07-24 01:18:03 -0500

    Added Repository Pattern,MemberDto's & AutoMapping

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Extensions;
 
 namespace API.Entities
 {
     public class AppUser
     {
         public int Id { get; set; }
-        [Required]
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+
+        public DateOnly DateOfBirth { get; set; }
+
+        public string KnownAs { get; set; }
+        public DateTime AccountCreated { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public List<Photo>  Photos { get; set; } = new List<Photo>();
+        // or
+        // public List<Photo>  Photos { get; set; } = new();
+
+
+        // Commenting the func to optimize automapper quering.. To reduce the load from database query and exclude passwordhash and password salt from query
+        // public int GetAge()
+        // {
+        //     return DateOfBirth.CalculateAge();
+        // }
+
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -53,4 +53,19 @@ app.UseAuthorization();   // what are you authorized to do with the valid token?
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+
+    logger.LogError(ex, "An error occured duing Migrations");
+}
+
 app.Run();
